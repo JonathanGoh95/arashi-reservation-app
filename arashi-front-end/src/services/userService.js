47efe046dea1774815d6ffbc1aa28102bd5d7f2a
Jonathan Goh <117988387+JonathanGoh95@users.noreply.github.com>
2025-06-25 12:55:13 +0800

    feat: Updated userService file

--- arashi-front-end/src/services/userService.js
+++ arashi-front-end/src/services/userService.js
@@ -1,11 +1,11 @@
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/users`;
 
-const index = async () => {
+const showUser = async (userId) => {
   try {
-    const res = await fetch(BASE_URL, {
+    const res = await fetch(`${BASE_URL}/${userId}`, {
       headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
     });
-
+    if (!res.ok) throw new Error("Failed to show user details");
     const data = await res.json();
 
     if (data.err) {
@@ -19,4 +19,46 @@ const index = async () => {
   }
 };
 
-export { index };
+const updateUser = async (userId, userFormData) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${userId}/edit`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userFormData),
+    });
+    if (!res.ok) throw new Error("Failed to update user details");
+    const data = await res.json();
+    if (data.err) {
+      throw new Error(data.err);
+    }
+    return data;
+  } catch (err) {
+    console.log(err);
+    throw new Error(err);
+  }
+};
+
+const deleteUser = async (userId) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${userId}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    if (!res.ok) throw new Error("Failed to delete user");
+    const data = await res.json();
+    if (data.err) {
+      throw new Error(data.err);
+    }
+    return data;
+  } catch (err) {
+    console.log(err);
+    throw new Error(err);
+  }
+};
+
+export { showUser, updateUser, deleteUser };
