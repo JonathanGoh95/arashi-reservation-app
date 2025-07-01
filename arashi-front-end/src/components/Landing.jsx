58d17130905d542d077c013db4d741f13339432d
Joanne <joannekjy@hotmail.com>
2025-07-01 10:18:12 +0800

    feat: import images

--- arashi-front-end/src/components/Landing.jsx
+++ arashi-front-end/src/components/Landing.jsx
@@ -1,3 +1,6 @@
+import restaurantImage1 from "../../images/restaurant photo 1.jpg"
+import restaurantImage2 from "../../images/restaurant photo.jpg"
+
 const stylePicture = {
   height: "400px", 
   width: "auto"
@@ -12,8 +15,8 @@ const Landing = () => {
             WELCOME TO ARASHI RESERVATIONS!
           </h1>
           <div className="is-flex is-justify-content-center">
-            <img className="card mr-3" style={stylePicture} src="./images/restaurant photo 1.jpg" ></img>
-            <img className="card ml-3" style={stylePicture} src="./images/restaurant photo.jpg" ></img>
+            <img className="card mr-3" style={stylePicture} src={restaurantImage1} ></img>
+            <img className="card ml-3" style={stylePicture} src={restaurantImage2} ></img>
           </div>
           <h1 className="m-4 is-italic has-text-black is-underlined">ABOUT US</h1>
           <p className="is-size-5 has-text-centered px-6">
